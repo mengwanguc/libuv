187ea0c0c95df7d0ef5eccb7d276ce2e018df7df
Meng Wang github desktop <mengwanguc@gmail.com>
2020-01-29 07:19:42 -0600

    debug

--- src/unix/stream.c
+++ src/unix/stream.c
@@ -1518,6 +1518,7 @@ int uv_write(uv_write_t* req,
              const uv_buf_t bufs[],
              unsigned int nbufs,
              uv_write_cb cb) {
+  printf("calling uv_write...\n);
   return uv_write2(req, handle, bufs, nbufs, NULL, cb);
 }
 
